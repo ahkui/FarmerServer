9b653c001853b49c2394767156415822c8dc556a
KUI\ahkui <ahkui@outlook.com>
2017-11-26 13:12:16 +0800

    add column

---
+++ database/migrations/2017_11_26_130817_add_original_address_datas_id_to_converted_address_datas_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddOriginalAddressDatasIdToConvertedAddressDatasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('converted_address_datas', function (Blueprint $table) {
+            $table->unsignedInteger('original_address_datas_id')->unique()->index('FK_converted_address_datas_original_address_datas_id');
+            $table->foreign('original_address_datas_id', 'FK_converted_address_datas_original_address_datas_id')->references('id')->on('original_address_datas')->onUpdate('CASCADE')->onDelete('CASCADE');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('converted_address_datas', function (Blueprint $table) {
+            $table->dropForeign('FK_converted_address_datas_original_address_datas_id');
+            $table->dropColumn('original_address_datas_id');
+        });
+    }
+}

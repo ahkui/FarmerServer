3953fd3c6596fb570ab90c6f459b614d21a9354b
KUI\ahkui <ahkui@outlook.com>
2018-01-02 00:01:44 +0800

    update

--- app/Jobs/ResetGoogleMapsApiCount.php
+++ app/Jobs/ResetGoogleMapsApiCount.php
@@ -3,6 +3,7 @@
 namespace App\Jobs;
 
 use App\GoogleMapsApi;
+use App\OriginalAddressData;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;

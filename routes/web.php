b502a88214ad36a8263b183ef3237ce1f7187bc6
KUI\ahkui <ahkui@outlook.com>
2018-01-02 01:00:02 +0800

    update

--- routes/web.php
+++ routes/web.php
@@ -33,58 +33,7 @@ Route::get('/home', 'HomeController@index')->name('home');
     use App\ConvertedAddressData;
 Route::get('now', function () {
     dump(
-        ConvertedAddressData::take(5)->get()
-    );
-    // dump(
-    //     ConvertedAddressData::where('location4', 'near', [
-    //         '$geometry' => [
-    //             'type' => 'Point',
-    //             'coordinates' => [
-    //                 121.516018,
-    //                 25.028516,
-    //             ],
-    //         ],
-    //         '$maxDistance' => 5000,
-    //     ])
-    // );
-    // dump(
-    //     ConvertedAddressData::where('location3', 'near', [
-    //         '$geometry' => [
-    //             'type' => 'Point',
-    //             'coordinates' => [
-    //                 121.516018,
-    //                 25.028516,
-    //             ],
-    //         ],
-    //         '$maxDistance' => 5000,
-    //     ])
-    // );
-    // dump(
-    //     ConvertedAddressData::where('location2', 'near', [
-    //         '$geometry' => [
-    //             'type' => 'Point',
-    //             'coordinates' => [
-    //                 121.516018,
-    //                 25.028516,
-    //             ],
-    //         ],
-    //         '$maxDistance' => 5000,
-    //     ])
-    // );
-    // dump(
-    //     ConvertedAddressData::where('location', 'near', [
-    //         '$geometry' => [
-    //             'type' => 'Point',
-    //             'coordinates' => [
-    //                 121.516018,
-    //                 25.028516,
-    //             ],
-    //         ],
-    //         '$maxDistance' => 5000,
-    //     ])
-    // );
-    dump(
-        ConvertedAddressData::where('location3', 'near', [
+        ConvertedAddressData::where('location', 'near', [
             '$geometry' => [
                 'type' => 'Point',
                 'coordinates' => [

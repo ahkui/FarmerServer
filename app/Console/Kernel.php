74a1f401e2965dbdd1ad82c3b882bcfb35bdc944
KUI\ahkui <ahkui@outlook.com>
2018-03-18 21:06:59 +0800

    remove auto convert

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,7 +27,6 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('convert:geo')->everyMinute();
         $schedule->command('location:generate')->daily();
         $schedule->job(new ResetGoogleMapsApiCount())->twiceDaily(0, 12);
         $schedule->job(new ConvertAddressToCoordinates())->everyMinute();

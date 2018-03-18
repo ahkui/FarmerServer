decd475ecbdad5f90c5a3ba92b016efa6bddbe35
MAXROG-PC\Max_L <MaxLye93@gmail.com>
2018-03-18 22:38:54 +0800

    created a map api return json

--- app/Http/Controllers/GeometryController.php
+++ app/Http/Controllers/GeometryController.php
@@ -49,6 +49,6 @@ class GeometryController extends Controller
                     ],
                 ],
             ],
-        ])->get(['name', 'location']);
+        ])->get(['name', 'location','types']);
     }
 }

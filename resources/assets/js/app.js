b50d625ae596d5538fef56f8102e958132f66e30
KUI\ahkui <ahkui@outlook.com>
2018-03-18 22:23:40 +0800

    update fontawesome

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -7,7 +7,7 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
-
+import fontawesome from '@fortawesome/fontawesome';
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application

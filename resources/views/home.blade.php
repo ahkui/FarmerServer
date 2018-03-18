f4ee40aa486410d0f3b945240b31567bf17874c7
KUI\ahkui <ahkui@outlook.com>
2018-03-18 22:16:23 +0800

    add fontawesome

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,5 @@
 @extends('layouts.app') @section('content')
 <div id="map"></div>
-<button type="button" onclick="window.clearMarker()" style="position: fixed;right: 0;bottom: 0;z-index: 9999;">remove all</button>
+<button class="rounded-circle" type="button" onclick="window.addCustomMarker()" style="position: fixed;left:40%;right: 1rem;bottom: 1rem;z-index: 9999;">＋</button>
+<button class="rounded-circle" type="button" onclick="window.addCustomMarker()" style="position: fixed;left:40%;right: 1rem;bottom: 1rem;z-index: 9999;"><i class="fas fa-search"></i></button>
 @endsection
